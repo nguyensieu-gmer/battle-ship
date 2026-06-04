37f1cbd2a23de2d3d046144af3a66d50b07cf80f
nguyensieu-gmer <217349416+nguyensieu-gmer@users.noreply.github.com>
2026-06-04 15:38:46 +0700

    Initial commit

---
+++ eslint.config.js
@@ -0,0 +1,33 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+
+export default defineConfig([
+  {
+    ignores: ['dist/**', 'node_modules/**'],
+  },
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+  },
+  {
+    rules: {
+      'no-unused-vars': 'warn',
+      'arrow-body-style': ['error', 'always'],
+      'no-undef': 'warn',
+    },
+  },
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      globals: {
+        ...globals.jest,
+      },
+    },
+  },
+
+  eslintConfigPrettier,
+]);
