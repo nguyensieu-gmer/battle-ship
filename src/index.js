60e59d4df6c17cc69c29a3ae720c8dc71580ffac
Nguyen Van Sieu <217349416+nguyensieu-gmer@users.noreply.github.com>
2026-06-04 18:45:17 +0700

    Add placeShip method and test valid place

--- src/index.js
+++ src/index.js
@@ -13,3 +13,44 @@ class Ship {
     return false;
   }
 }
+
+function createNullMatrix(row, col) {
+  const matrix = [];
+  for (let i = 0; i < row; i++) {
+    matrix[i] = new Array(col).fill(null);
+  }
+  return matrix;
+}
+
+class Gameboard {
+  constructor() {
+    this.row = 10;
+    this.col = 10;
+    this.watter = createNullMatrix(this.row, this.col);
+  }
+  placeAShip(x, y, ship) {
+    //place ship in horizontal
+    if (x < 0 || x > this.row - 1 || y < 0 || y > this.col - 1) {
+      return;
+    }
+    if (y + ship.len - 1 > this.row - 1) {
+      return;
+    }
+    if (!this.canPlace(x, y, ship)) {
+      return;
+    }
+    for (let i = 0; i < ship.len; i++) {
+      this.watter[x][y + i] = ship;
+    }
+  }
+  canPlace(x, y, ship) {
+    for (let i = 0; i < ship.len; i++) {
+      if (this.watter[x][y + i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
+
+export { Ship, Gameboard };
