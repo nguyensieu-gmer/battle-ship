05d00225b9ef76bb2c719d00c2a960978add33e5
Nguyen Van Sieu <217349416+nguyensieu-gmer@users.noreply.github.com>
2026-06-04 15:50:23 +0700

    Add ship class

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-
+class Ship {
+  constructor(len) {
+    this.len = len;
+    this.hitted = 0;
+  }
+  hit() {
+    this.hitted += 1;
+  }
+  isSunk() {
+    if (this.hitted >= this.len) {
+      return true;
+    }
+    return false;
+  }
+}
