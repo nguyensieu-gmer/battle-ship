c2c1569bf9727e5549e8ecede8c51e688c88c18c
Nguyen Van Sieu <217349416+nguyensieu-gmer@users.noreply.github.com>
2026-06-06 10:35:28 +0700

    Make render board module

--- src/render.js
+++ src/render.js
@@ -2,6 +2,54 @@ class Render {
   constructor(player) {
     this.container = document.querySelector('.container');
     this.player = player;
+    this.renderIntoScreen();
+    this.displaymaxtrix();
   }
-  renderBoard(board) {}
+  displaymaxtrix() {
+    console.log(this.player.realPlayer.watter);
+    console.log(this.player.computer.watter);
+  }
+  renderPlayerBoard(playerBoard, playerName, id, shipColor) {
+    const player1 = document.createElement('div');
+    player1.id = id;
+    player1.classList.add('player');
+    const name = document.createElement('h1');
+    name.classList.add('name');
+    name.textContent = playerName;
+    player1.appendChild(name);
+    const gridContainer = document.createElement('div');
+    gridContainer.classList.add('grid_container');
+
+    for (let i = 0; i < playerBoard.length; i++) {
+      for (let j = 0; j < playerBoard[0].length; j++) {
+        const cell = document.createElement('div');
+        if (playerBoard[i][j]) cell.classList.add(shipColor);
+        cell.classList.add('cell');
+        cell.dataset.row = i;
+        cell.dataset.col = j;
+        gridContainer.appendChild(cell);
+      }
+    }
+    player1.appendChild(gridContainer);
+    return player1;
+  }
+  renderIntoScreen() {
+    this.container.innerHTML = '';
+    const player = this.renderPlayerBoard(
+      this.player.realPlayer.watter,
+      'Player',
+      'realplayer',
+      'frendShip',
+    );
+    const computer = this.renderPlayerBoard(
+      this.player.computer.watter,
+      'Computer',
+      'computer',
+      'enemyShip',
+    );
+    this.container.appendChild(player);
+    this.container.appendChild(computer);
+  }
 }
+
+export { Render };
