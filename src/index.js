5de055fd96fad65245186b2dd494a6d364b57fae
Nguyen Van Sieu <217349416+nguyensieu-gmer@users.noreply.github.com>
2026-06-07 15:21:01 +0700

    render default

--- src/index.js
+++ src/index.js
@@ -2,5 +2,35 @@ import { Player } from './game_board.js';
 import { Render } from './render.js';
 import './style.css';
 
-const player = new Player();
-new Render(player);
+class Controller {
+  constructor() {
+    this.player = new Player();
+    this.render = new Render();
+    this.enemyZone = document.querySelector('#computer .grid_container');
+    this.playerAttack = null;
+    this.computerAttack = this.randomAttack();
+    this.playerTurn = true;
+
+    this.render.renderIntoScreen(this.player);
+    this.bindEvent();
+  }
+  resetBoard() {
+    this.render.renderIntoScreen(this.player);
+  }
+  bindEvent() {
+    this.enemyZone.addEventListener('click', (e) => {
+      const cell = e.target.closest('.cell');
+      if (!cell) return;
+      const x = cell.dataset.row;
+      const y = cell.dataset.col;
+      this.playerAttack = [x, y];
+    });
+  }
+  randomAttack() {
+    const x = Math.floor(Math.random() * 10);
+    const y = Math.floor(Math.random() * 10);
+    return [x, y];
+  }
+}
+
+new Controller();
