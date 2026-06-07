d7e878d3b3de2898ad4773577396d8ba83696e47
Nguyen Van Sieu <217349416+nguyensieu-gmer@users.noreply.github.com>
2026-06-07 20:16:19 +0700

    Make attack feature

--- src/index.js
+++ src/index.js
@@ -9,10 +9,6 @@ class Controller {
     this.render.renderIntoScreen(this.player);
     this.enemyZone = document.querySelector('#computer .grid_container');
 
-    this.playerAttack = null;
-    this.computerAttack = this.randomAttack();
-    this.playerTurn = true;
-
     this.init();
   }
   init() {
@@ -27,13 +23,25 @@ class Controller {
       if (!cell) return;
       const x = cell.dataset.row;
       const y = cell.dataset.col;
-      this.playerAttack = [x, y];
+      const success = this.player.computer.receiveAttack(x, y);
+      if (!success) return;
+      this.computerAttack();
+      this.render.markAttackForCell(cell);
     });
   }
   randomAttack() {
     const x = Math.floor(Math.random() * 10);
     const y = Math.floor(Math.random() * 10);
     return [x, y];
+  }
+  computerAttack() {
+    let [x, y] = this.randomAttack();
+    while (this.player.realPlayer.attacked[x][y] === 1) {
+      [x, y] = this.randomAttack();
+    }
+    this.player.realPlayer.receiveAttack(x, y);
+    const cell = document.querySelector(`[data-row='${x}'][data-col='${y}']`);
+    this.render.markAttackForCell(cell);
   }
 }
 
