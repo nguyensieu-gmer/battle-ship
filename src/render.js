fb1eebe6575606afc1e6505d5a45d5def1a05372
Nguyen Van Sieu <217349416+nguyensieu-gmer@users.noreply.github.com>
2026-06-08 10:40:15 +0700

    Add render each board method

--- src/render.js
+++ src/render.js
@@ -1,6 +1,10 @@
 class Render {
   constructor() {
     this.container = document.querySelector('.container');
+    this.friendShip = 'friendShip';
+    this.enemyShip = 'enemyShip';
+    this.friend = 'friend';
+    this.enemy = 'enemy';
   }
   makeMarkAttack() {
     const attack = document.createElement('div');
@@ -16,6 +20,7 @@ class Render {
     name.textContent = playerName;
     player1.appendChild(name);
     const gridContainer = document.createElement('div');
+    gridContainer.id = shipColor === this.friendShip ? this.friend : this.enemy;
     gridContainer.classList.add('grid_container');
     for (let i = 0; i < playerBoard.length; i++) {
       for (let j = 0; j < playerBoard[0].length; j++) {
@@ -41,14 +46,14 @@ class Render {
       player.realPlayer.attacked,
       'Player',
       'realplayer',
-      'frendShip',
+      this.friendShip,
     );
     const computer = this.renderPlayerBoard(
       player.computer.water,
       player.computer.attacked,
       'Computer',
       'computer',
-      'enemyShip',
+      this.enemyShip,
     );
     this.container.appendChild(real);
     this.container.appendChild(computer);
@@ -56,6 +61,26 @@ class Render {
   markAttackForCell(cell) {
     const attack = this.makeMarkAttack();
     cell.appendChild(attack);
+  }
+  renderBoard(player, container) {
+    container.innerHTML = '';
+    const matrix = player.water;
+    for (let i = 0; i < matrix.length; i++) {
+      for (let j = 0; j < matrix[0].length; j++) {
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        const shipColor =
+          container.id === this.friend ? this.friendShip : this.enemyShip;
+        if (matrix[i][j]) cell.classList.add(shipColor);
+        if (player.attacked[i][j] === 1) {
+          const attack = this.makeMarkAttack();
+          cell.appendChild(attack);
+        }
+        cell.dataset.row = i;
+        cell.dataset.col = j;
+        container.appendChild(cell);
+      }
+    }
   }
 }
 
