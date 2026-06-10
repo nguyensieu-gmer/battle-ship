0ecb327d350c03230909fd2da166ebc4d2be7a02
Nguyen Van Sieu <217349416+nguyensieu-gmer@users.noreply.github.com>
2026-06-10 08:26:36 +0700

    Itergrate first put ship to controller class

--- src/index.js
+++ src/index.js
@@ -8,11 +8,65 @@ class Controller {
     this.render = new Render();
     this.enemyZone = null; // after render competion
     this.friendZone = null; //  after render competion
+    this.previewCell = [];
+    this.shipsElement = null;
+    this.chooseableArea = null;
+    this.currentShip = null;
 
     this.winnerDialog = document.getElementById('winner_dialog'); // in template
     this.winner = document.getElementById('winner'); // in template
 
-    this.initAfterChoose(); // it will not init first
+    this.firstInit();
+  }
+  firstInit() {
+    this.render.showShipPlacement(this.player.realPlayer);
+    this.previewCell = [];
+    this.shipsElement = document.querySelectorAll('.ship');
+    this.chooseableArea = document.querySelector('.main_board .grid_container');
+    this.currentShip = null;
+    this.bindEventFirst();
+  }
+  bindEventFirst() {
+    this.shipsElement.forEach((ship) => {
+      ship.addEventListener('dragstart', () => {
+        ship.classList.add('dragging');
+        const dragging = document.querySelector('.dragging');
+        this.currentShip = new Ship(Number(dragging.dataset.value));
+      });
+
+      ship.addEventListener('dragend', () => {
+        this.currentShip = null;
+        ship.classList.remove('dragging');
+      });
+    });
+    this.chooseableArea.addEventListener('dragover', (e) => {
+      e.preventDefault();
+      const cell = e.target.closest('.cell');
+      if (!cell) return;
+      const x = Number(cell.dataset.row);
+      const y = Number(cell.dataset.col);
+      this.showPreview(x, y, this.currentShip);
+    });
+    this.chooseableArea.addEventListener('dragleave', () => {
+      this.clearPreview();
+    });
+    this.chooseableArea.addEventListener('drop', (e) => {
+      const cell = e.target.closest('.cell');
+      if (!cell) return;
+      if (!this.currentShip) return;
+      const x = Number(cell.dataset.row);
+      const y = Number(cell.dataset.col);
+      const put = this.player.realPlayer.placeAShip(x, y, this.currentShip);
+      this.previewCell.forEach((c) => {
+        const x = Number(c.dataset.row);
+        const y = Number(c.dataset.col);
+        this.player.realPlayer.occupyACell(x, y);
+      });
+      this.render.renderBoard(this.player.realPlayer, this.chooseableArea);
+      const dragging = document.querySelector('.dragging');
+      if (put) dragging.remove();
+      this.clearPreview();
+    });
   }
   initAfterChoose() {
     this.render.renderIntoScreen(this.player);
@@ -71,84 +125,32 @@ class Controller {
       this.winnerDialog.showModal();
     }
   }
+  showPreview(x, y, ship) {
+    this.clearPreview();
+    let cells = [];
+    for (let i = 0; i < ship.len; i++) {
+      let nx = x;
+      let ny = y + i;
+      if (nx >= 10 || ny >= 10) return;
+      const cell = document.querySelector(
+        `[data-row='${nx}'][data-col='${ny}']`,
+      );
+      cells.push(cell);
+    }
+    const valid = cells.every((c) => {
+      return !c.classList.contains('occupied');
+    });
+    cells.forEach((c) => {
+      c.classList.add(valid ? 'preview' : 'invalid');
+    });
+    this.previewCell = cells;
+  }
+  clearPreview() {
+    this.previewCell.forEach((c) => {
+      c.classList.remove('preview', 'invalid');
+    });
+    this.previewCell = [];
+  }
 }
 
 new Controller();
-
-// const render = new Render();
-// const player = new Player();
-// render.showShipPlacement(player.realPlayer);
-
-// let previewCell = [];
-// const shipsElement = document.querySelectorAll('.ship');
-// const chooseableArea = document.querySelector('.main_board .grid_container');
-// let currentShip = null;
-
-// shipsElement.forEach((ship) => {
-//   ship.addEventListener('dragstart', () => {
-//     ship.classList.add('dragging');
-//     const dragging = document.querySelector('.dragging');
-//     currentShip = new Ship(Number(dragging.dataset.value));
-//   });
-
-//   ship.addEventListener('dragend', () => {
-//     ship.classList.remove('dragging');
-//   });
-// });
-
-// chooseableArea.addEventListener('dragover', (e) => {
-//   e.preventDefault();
-//   const cell = e.target.closest('.cell');
-//   if (!cell) return;
-//   const x = Number(cell.dataset.row);
-//   const y = Number(cell.dataset.col);
-//   showPreview(x, y, currentShip);
-// });
-
-// function showPreview(x, y, ship) {
-//   clearPreview();
-//   let cells = [];
-//   for (let i = 0; i < ship.len; i++) {
-//     let nx = x;
-//     let ny = y + i;
-//     if (nx >= 10 || ny >= 10) return;
-//     const cell = document.querySelector(`[data-row='${nx}'][data-col='${ny}']`);
-//     cells.push(cell);
-//   }
-//   const valid = cells.every((c) => {
-//     return !c.classList.contains('occupied');
-//   });
-//   cells.forEach((c) => {
-//     c.classList.add(valid ? 'preview' : 'invalid');
-//   });
-//   previewCell = cells;
-// }
-
-// function clearPreview() {
-//   previewCell.forEach((c) => {
-//     c.classList.remove('preview', 'invalid');
-//   });
-//   previewCell = [];
-// }
-
-// chooseableArea.addEventListener('dragleave', () => {
-//   clearPreview();
-// });
-
-// chooseableArea.addEventListener('drop', (e) => {
-//   const cell = e.target.closest('.cell');
-//   if (!cell) return;
-//   if (!currentShip) return;
-//   const x = Number(cell.dataset.row);
-//   const y = Number(cell.dataset.col);
-//   const put = player.realPlayer.placeAShip(x, y, currentShip);
-//   previewCell.forEach((c) => {
-//     const x = Number(c.dataset.row);
-//     const y = Number(c.dataset.col);
-//     player.realPlayer.occupyACell(x, y);
-//   });
-//   render.renderBoard(player.realPlayer, chooseableArea);
-//   const dragging = document.querySelector('.dragging');
-//   if (put) dragging.remove();
-//   clearPreview();
-// });
