7160f6f6844db855f37db839ede5d25ff22aff6f
Nguyen Van Sieu <217349416+nguyensieu-gmer@users.noreply.github.com>
2026-06-12 09:50:58 +0700

    Add test for y_axis

--- test/game_board.test.js
+++ test/game_board.test.js
@@ -22,47 +22,56 @@ test('test water', () => {
 
 describe('placeAShip test', () => {
   test('placeAShip method with edge case', () => {
-    expect(board.placeAShip(-1, -1, mockShip)).toBe(false);
-    expect(board.placeAShip(10, 10, mockShip)).toBe(false);
+    expect(board.placeAShip(-1, -1, mockShip, true)).toBe(false);
+    expect(board.placeAShip(10, 10, mockShip, true)).toBe(false);
   });
 
   test('ship cant fit water place', () => {
-    expect(board.placeAShip(0, 9, mockShip)).toBe(false);
-    expect(board.placeAShip(0, 9, mockShip)).toBe(false);
+    expect(board.placeAShip(0, 9, mockShip, true)).toBe(false);
+    expect(board.placeAShip(0, 9, mockShip, true)).toBe(false);
   });
 
   test('place ship', () => {
-    board.placeAShip(0, 0, mockShip);
+    board.placeAShip(0, 0, mockShip, true);
     expect(board.water[0][0]).toEqual(mockShip);
     expect(board.water[0][1]).toEqual(mockShip);
     expect(board.water[0][2]).toEqual(mockShip);
     expect(board.water[0][3]).toEqual(null);
   });
+  test('y-asix', () => {
+    board.placeAShip(0, 0, mockShip, false);
+    expect(board.water[1][0]).toEqual(mockShip);
+    expect(board.water[2][0]).toEqual(mockShip);
+  });
+  test('y-asix out of bound', () => {
+    const success = board.placeAShip(8, 0, mockShip, false);
+    expect(success).toBe(false);
+  });
 });
 
 test('hit a target', () => {
-  board.placeAShip(5, 3, mockShip);
+  board.placeAShip(5, 3, mockShip, true);
   board.receiveAttack(5, 4);
   expect(mockShip.hit).toHaveBeenCalled();
   expect(board.attacked[5][4]).toBe(1);
 });
 
 test('test all sunk method', () => {
-  board.placeAShip(5, 3, mockShip);
+  board.placeAShip(5, 3, mockShip, true);
   board.receiveAttack(5, 4);
   expect(mockShip.isSunk).toHaveBeenCalled();
 });
 
 test('test with 3 ship', () => {
-  board.placeAShip(0, 0, mockShip);
-  board.placeAShip(1, 0, mockShip);
+  board.placeAShip(0, 0, mockShip, true);
+  board.placeAShip(1, 0, mockShip, true);
   expect(board.putEnoughShip(3)).toBe(false);
-  board.placeAShip(2, 0, mockShip);
+  board.placeAShip(2, 0, mockShip, true);
   expect(board.putEnoughShip(3)).toBe(true);
 });
 
 test('a ship is sink or not', () => {
-  board.placeAShip(0, 0, mockShip);
+  board.placeAShip(0, 0, mockShip, true);
   board.receiveAttack(0, 1);
   board.isShipSunk(0, 0);
   expect(mockShip.isSunk).toHaveBeenCalled();
@@ -76,8 +85,8 @@ test('dfs', () => {
     isSunk: jest.fn(),
   };
   board = new Gameboard();
-  board.placeAShip(0, 1, mockShip);
-  board.placeAShip(1, 0, mockShip1);
+  board.placeAShip(0, 1, mockShip, true);
+  board.placeAShip(1, 0, mockShip1, true);
   const cells = board.findAdjacentShip(0, 3, mockShip);
   expect(cells.length).toBe(3);
   // check contain
